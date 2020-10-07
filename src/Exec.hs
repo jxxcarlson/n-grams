b2f62a6dee86fc190e81471fb0a00400dabbb8c1
James Carlson <jxxcarlson@gmail.com>
2020-10-07 08:13:17 -0400

    Add commans:
      * rfreq: relative frequency
    
      * lfreq: log relative frequency

--- src/Exec.hs
+++ src/Exec.hs
@@ -1,6 +1,6 @@
 module Exec (exec) where
 
-import Frequency (wordcount, linecount, getSortedWords )
+import Frequency (wordcount, linecount, getSortedWords, relativeFrequencies )
 import Utility ((|>))
 
 exec :: String -> IO ()
@@ -12,6 +12,8 @@ exec str =
          "help" -> help
          "stats" -> stats args
          "freq" -> freq args
+         "rfreq" -> relFreq args
+         "lfreq" -> logRelFreq args
          _ -> putStrLn "??"
 
 
@@ -20,9 +22,11 @@ help =
   do
     putStrLn ""
     putStrLn "  Commands"
-    putStrLn "  ----------------------------"
-    putStrLn "  stats FILENAME"
-    putStrLn "  freq  FILENAME START HOWMANY"
+    putStrLn "  ------------------------------------------------------------"
+    putStrLn "  stats FILENAME                      word, line, type count"
+    putStrLn "  freq  FILENAME START HOWMANYb       word frequencies"
+    putStrLn "  rfreq  FILENAME START HOWMANY       relative frequencies"
+    putStrLn "  lfreq  FILENAME START HOWMANY       log relative requencies"
     putStrLn ""
 
 
@@ -34,7 +38,7 @@ stats args =
   do  
     contents <- readFile filePath
     putStrLn ""
-    putStrLn "File:  whitman.txt" 
+    putStrLn $ "File: " ++ filePath 
     putStrLn $ "Words: " ++ show (wordcount contents)
     putStrLn $ "Types: " ++ (show $ length $ getSortedWords contents)
     putStrLn $ "Lines: " ++ show (linecount contents)
@@ -51,10 +55,44 @@ freq args =
   do  
     contents <- readFile filePath
     putStrLn ""
-    putStrLn "File:  whitman.txt" 
+    putStrLn $ "File: " ++ filePath 
     putStrLn ""
     putStrLn $ howMany ++ " words from index " ++ start ++ ": " -- ++ show (take (read howMany)$ drop (read start) $ getSortedWords contents)
-    putStrLn $ formatFrequenceList $ sliceSortedWords (read start) (read howMany) contents   -- (take (read howMany)$ drop (read start) $ getSortedWords contents)
+    putStrLn $ formatIntPairs $ sliceSortedWords (read start) (read howMany) contents   -- (take (read howMany)$ drop (read start) $ getSortedWords contents)
+    putStrLn ""
+
+
+relFreq :: [String] -> IO ()
+relFreq args = 
+  let
+    filePath = args !! 0
+    start = args !! 1
+    howMany = args !! 2
+  in  
+  do  
+    contents <- readFile filePath
+    putStrLn ""
+    putStrLn $ "File: " ++ filePath 
+    putStrLn ""
+    putStrLn $ howMany ++ " words from index " ++ start ++ ": " -- ++ show (take (read howMany)$ drop (read start) $ getSortedWords contents)
+    putStrLn $ formatDoublePairs $ relativeFrequencies $ sliceSortedWords (read start) (read howMany) contents   -- (take (read howMany)$ drop (read start) $ getSortedWords contents)
+    putStrLn ""
+
+
+logRelFreq :: [String] -> IO ()
+logRelFreq args = 
+  let
+    filePath = args !! 0
+    start = args !! 1
+    howMany = args !! 2
+  in  
+  do  
+    contents <- readFile filePath
+    putStrLn ""
+    putStrLn $ "File: " ++ filePath 
+    putStrLn ""
+    putStrLn $ howMany ++ " words from index " ++ start ++ ": " -- ++ show (take (read howMany)$ drop (read start) $ getSortedWords contents)
+    putStrLn $ formatDoublePairs $ map (\(a,b) -> (a, -log b)) $ relativeFrequencies $ sliceSortedWords (read start) (read howMany) contents   -- (take (read howMany)$ drop (read start) $ getSortedWords contents)
     putStrLn ""
 
 
@@ -62,15 +100,21 @@ sliceSortedWords :: Int -> Int ->  String -> [(String, Int)]
 sliceSortedWords start howMany text =
   take howMany $ drop start $ getSortedWords text
 
-formatFrequenceList :: [(String, Int)] -> String
-formatFrequenceList pairs = 
-  foldr (\(s, k) acc -> (padRight 10 s ++ show k ++ "\n" ++ acc)) "" pairs
+formatIntPairs :: [(String, Int)] -> String
+formatIntPairs pairs = 
+  foldr (\(s, k) acc -> (padRight 15 s ++ show k ++ "\n" ++ acc)) "" pairs
+
+
+formatDoublePairs :: [(String, Double)] -> String
+formatDoublePairs pairs = 
+  foldr (\(s, k) acc -> (padRight 15 s ++ show k ++ "\n" ++ acc)) "" pairs
+
+-- HELPERS
 
 padRight :: Int -> String -> String
 padRight i str =
   let
     n = length str
     j = max (i - n) 0
-    blanks = take j $ repeat ' '
-  in
+    blanks = take j $ repeat ' ' in
     str ++ blanks
