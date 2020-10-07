7245b6503a173ea09c8af00b80bfb367064e144e
James Carlson <jxxcarlson@gmail.com>
2020-10-07 15:33:21 -0400

    Delete unneeded code

--- app/Main.hs
+++ app/Main.hs
@@ -31,29 +31,3 @@ eval' input =
 
 print' :: String -> IO ()
 print' = putStrLn
-
-
-
-
-   
-
--- main :: IO ()
--- main = do  
---     handle <- openFile "whitman.txt" ReadMode  
---     contents <- hGetContents handle 
---     putStrLn ""
---     putStrLn "File:  whitman.txt" 
---     putStrLn $ "Words: " ++ show (wordcount contents)
---     putStrLn $ "Types: " ++ (show $ length $ getSortedWords contents)
---     putStrLn $ "Lines: " ++ show (linecount contents)
---     putStrLn ""
---     putStrLn "start: "
---     start <- getLine
---     putStrLn "number of words: "
---     howMany <- getLine
---     putStrLn ""
---     putStrLn $ howMany ++ " words from index " ++ start ++ ": " ++ show (take (read howMany)$ drop (read start) $ getSortedWords contents)
---     putStrLn ""
---     hClose handle 
-
-   
